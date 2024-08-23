ee1d59079b36674ee98574d9bea625ad7f7d13cc
nicksementsov <nickolaisementsov@gmail.com>
2024-08-23 15:55:45 -0400

    Messing around with the polynomial graph.

--- 2 - Regression/2 - 3 Polynomial Regression/2 - 3 Polynomial Regression R.R
+++ 2 - Regression/2 - 3 Polynomial Regression/2 - 3 Polynomial Regression R.R
@@ -2,16 +2,9 @@
 
 ## Setup
 setwd('~/Documents/R/2 - Regression/2 - 3 Polynomial Regression')
-library(caTools)
 library(ggplot2)
-set.seed(123)
 dataSet = read.csv('Position_Salaries.csv')
 dataSet = dataSet[2:3]
-
-## Creating Training & Testing Sets
-split = sample.split(dataSet$Salary, SplitRatio = 2/3)
-training_set = subset(dataSet, split == TRUE)
-test_set = subset(dataSet, split == FALSE)
 
 ## Fit Linear Regressor to dataSet
 lin_reg = lm(formula = Salary ~ .,
@@ -49,12 +42,15 @@ x_grid = seq(min(dataSet$Level), max(dataSet$Level), 0.1)
 ggplot() +
   geom_point(aes(x = dataSet$Level, y = dataSet$Salary),
              colour = 'red') +
-  geom_line(aes(x = x_grid, y = predict(poly_reg,
-                                        newdata = data.frame(Level = x_grid,
-                                                             Level2 = x_grid^2,
-                                                             Level3 = x_grid^3,
-                                                             Level4 = x_grid^4))),
-            colour = 'blue') +
+  geom_line(aes(x = x_grid,
+                y = predict(poly_reg,
+                            newdata = data.frame(Level = x_grid,
+                                                 Level2 = x_grid^2,
+                                                 Level3 = x_grid^3,
+                                                 Level4 = x_grid^4))),
+            colour = 'purple') +
+  geom_line(aes(x = dataSet$Level, y = predict(lin_reg, newdata = dataSet)),
+            colour = 'green') +
   ggtitle('Position vs Salary (polynomial regression') +
   xlab('Position') +
   ylab('Salary')
